404a666c7904e728f3e6f53b318ef95bd98ad368
Markus Dethlefsen <Drathal@users.noreply.github.com>
2018-08-01 22:03:27 +0200

    some small refactoring

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import './styles.css'
 
 const names = ['Andre', 'Maike 🦄', 'Lior', 'Martin', 'Kevin', 'Byeongsoo', 'Torben', 'Markus']
 
-const count = names.length
-const pagecount = count + 1
+let currentPage = 0
+const nameCount = names.length
+const pageCount = nameCount + 2
 const getIndex = () => Math.floor(Math.random() * Math.floor(names.length))
 const getName = () => names.splice(getIndex(), 1)[0] || ''
-
 const getGradientIndex = () => Math.floor(Math.random() * Math.floor(4))
-const getTitle = i => (i === 0 ? 'Today we start with' : i === count - 1 ? 'last but not least' : 'Next up')
+const getTitle = i => (i === 0 ? 'Today we start with' : i === nameCount - 1 ? 'last but not least' : 'Next up')
 
 const Page = ({ offset, title, name = getName(), onClick, gradient = getGradientIndex() }) => (
   <React.Fragment>
@@ -34,34 +34,34 @@ const Page = ({ offset, title, name = getName(), onClick, gradient = getGradient
 )
 
 class App extends React.Component {
-  currentPage = 0
   scroll = to => this.refs.parallax.scrollTo(to)
   scrollTo = to => () => {
-    this.currentPage = to
-    this.scroll(this.currentPage)
+    currentPage = to
+    this.scroll(currentPage)
   }
-  onKeyDown = e => {
+  onKeyPressed = e => {
+    let offset = 0
     switch (e.keyCode) {
       case 37:
-        this.currentPage -= 1
+        offset = -1
         break
       case 13:
       case 39:
-        this.currentPage += 1
+        offset = 1
         break
       default:
         break
     }
-    this.currentPage = (this.currentPage + pagecount) % pagecount
-    this.scroll(this.currentPage)
+    currentPage = (currentPage + pageCount + offset) % pageCount
+    this.scroll(currentPage)
   }
   render() {
     return (
-      <div tabIndex={'0'} onKeyDown={this.onKeyDown}>
-        <Parallax className="container" ref="parallax" pages={count + 2} horizontal scrolling={false}>
+      <div tabIndex={'0'} onKeyUp={this.onKeyPressed}>
+        <Parallax className="container" ref="parallax" pages={pageCount} horizontal scrolling={false}>
           <Page offset={0} title={'welcome'} name={''} onClick={this.scrollTo(1)} />
           {names.map((_, i) => <Page offset={i + 1} title={getTitle(i)} onClick={this.scrollTo(i + 2)} key={i} />)}
-          <Page offset={count + 1} title={'Have a nice day'} name={''} onClick={this.scrollTo(0)} />
+          <Page offset={pageCount - 1} title={'Have a nice day'} name={''} onClick={this.scrollTo(0)} />
         </Parallax>
       </div>
     )
